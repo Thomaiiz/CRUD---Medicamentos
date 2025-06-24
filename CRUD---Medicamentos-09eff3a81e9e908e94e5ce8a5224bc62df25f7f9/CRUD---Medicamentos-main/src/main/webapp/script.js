04b717c378de3b4a2bb990f91aa47f251125a7fd
Thomaz <thomazsegreto@gmail.com>
2025-06-24 11:57:32 -0300

    arrumando os gits

--- CRUD---Medicamentos-09eff3a81e9e908e94e5ce8a5224bc62df25f7f9/CRUD---Medicamentos-main/src/main/webapp/script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('header nav');
-
-toggle.addEventListener('click', () => {
-  nav.classList.toggle('open');
-});
